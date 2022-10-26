093df99c870393903cd17b3929053dcd97217415
Julia <j.spivakovska@gmail.com>
2022-10-26 23:33:59 +0300

    1

--- js/task-06.js
+++ js/task-06.js
@@ -3,24 +3,21 @@ const refs = {
     dataLength: document.querySelector('#validation-input').getAttribute('data-length'),
 }
 
-const checkInput = (event) => {
-    if (Number(refs.dataLength) === refs.input.value.length) {
-        refs.input.classList.add('valid')
-    } 
+const checkInput = event => {
+  if (Number(refs.dataLength) === refs.input.value.length) {
+    refs.input.classList.add('valid');
+  } else {
+    refs.input.classList.add('invalid');
+  }
 
-    if (Number(refs.dataLength) !== refs.input.value.length) {
-        refs.input.classList.add('invalid')
-    }
+  if (
+    Number(refs.dataLength) === refs.input.value.length &&
+    refs.input.classList.contains('invalid')
+  ) {
+    refs.input.classList.replace('invalid', 'valid');
+  } else {
+    refs.input.classList.replace('valid', 'invalid');
+  }
+};
 
-    if (Number(refs.dataLength) === refs.input.value.length
-        && refs.input.classList.contains('invalid')) {
-        refs.input.classList.replace('invalid', 'valid')
-    }
-    
-    if (Number(refs.dataLength) !== refs.input.value.length
-        && refs.input.classList.contains('valid')) {
-        refs.input.classList.replace('valid', 'invalid')
-    }        
-}
-
-refs.input.addEventListener('blur', checkInput)
+refs.input.addEventListener('blur', checkInput);
--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,22 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+};
 
-const onClickSubmit3 = (event) => { 
-    event.preventDefault();   
+const onClickSubmit3 = event => {
+  event.preventDefault();
 
-    if (refs.form.elements.email.value !== '' && refs.form.elements.password.value !== '') {
-        const formValues = {
-            email: refs.form.elements.email.value,
-            password: refs.form.elements.password.value,
-        }
+  if (refs.form.elements.email.value !== '' && refs.form.elements.password.value !== '') {
+    const formValues = {
+      email: refs.form.elements.email.value,
+      password: refs.form.elements.password.value,
+    };
 
-        event.currentTarget.reset();
+    event.currentTarget.reset();
 
-        return console.log(formValues);
-    }
+    return console.log(formValues);
+  }
 
-    window.alert('Увага! Всі поля повинні бути заповнені!')
-} 
+  alert('Все поля должны быть заполнены.');
+};
+
+refs.form.addEventListener('submit', onClickSubmit3);
